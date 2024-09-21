2d2aacaaa2d18d57ab998896897349f116bfbb12
diananov11 <ohlemon0678@gmail.com>
2024-09-21 12:56:54 +0700

    create navbar

---
+++ src/components/CartList.jsx
@@ -0,0 +1,11 @@
+
+
+const CartList = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default CartList
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import { FaShoppingCart } from "react-icons/fa";
+
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className="navbar bg-body-tertiary container-fluid">
+        <div className="container flex">
+          <div className="">
+            <a className="navbar-brand" href="#">
+              <span>✨</span>
+              UrbanForge
+            </a>
+          </div>
+          <div>
+            <small className=" position-relative">
+              <FaShoppingCart size={20} />
+              <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                3
+                <span className="visually-hidden">cart total</span>
+              </span>
+            </small>
+          </div>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar
